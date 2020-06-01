ba8e09089f5f0dd48d811836626b376417517f69
alfonso-sosa <alfonso.sosa@hulu.com>
2020-06-01 08:17:39 -0700

    Add Stream flv

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import flv from 'flv.js';
 import {connect} from 'react-redux';
 import {isNullOrUndefined} from 'util';
 import {fetchStream} from '../../actions/streams';
@@ -6,9 +7,37 @@ import {fetchStream} from '../../actions/streams';
 
 class StreamShow extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
     const {fetchStream, match} = this.props;
-    fetchStream(match.params.id);
+    const {id} = match.params;
+    fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    const {id} = this.props.match.params;
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
@@ -21,6 +50,7 @@ class StreamShow extends React.Component {
       <div>
         <h1>{title}</h1>
         <h3>{description}</h3>
+        <video ref={this.videoRef} style={{width: '100%'}} controls={true} />
       </div>
     );
   }
